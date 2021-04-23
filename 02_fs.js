84dcc31e831411c989e331d67f8950e2e90d37d4
akhtar02 <akhtarhussain0788@gmail.com>
2021-04-23 20:31:01 +0500

    Basic Nodejs

---
+++ 02_fs.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+
+
+// // ** SYNC **
+// console.log ('SYNC 1')
+// files = fs.readdirSync('./');  // 10min IO BLOCK
+// console.log('SYNC 2', files);
+
+
+// // ** ASYNC **
+console.log('ASYNC 1');
+fs.readdir('./', function (err, files) {
+    if (err) {
+        console.log('Err: ', err)
+    } else {
+        console.log('Files: ', files);
+    }
+})
+console.log('ASYNC 2');
